e7bd9b1bd1024fc9547966366d2541bc2d34a901
chunjie <30278577+ChunjieShawn@users.noreply.github.com>
2022-06-01 22:47:26 +0800

    Update heelo.nix

--- heelo/heelo.nix
+++ heelo/heelo.nix
@@ -1,8 +1,15 @@
-{ stdenv, fetchgit }:
-
-stdenv.mkDerivation {
+let pkgs =  import <nixpkgs>{
+    config.allowBroken = true;
+    config.allowUnfree = true;
+};
+in
+pkgs.python39Packages.buildPythonPackage {
   name = "heelo-0.0.0";
-  src = fetchgit {
+
+  buildInputs = [ pkgs.perl ];
+  src = pkgs.fetchgit {
     url = "https://github.com/ChunjieShawn/pybuild.git";
+    rev = "22d3545b757ac9c6e84003b3614bac24c8146fb0";
+    sha256 = "sha256-SuqdfR3jU+4ME2wDEuz1RwMRZf+l70fH96liyQwd+6c=";
   };
 }
